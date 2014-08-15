fd2d84fb37cc559f90fda76e3bc85b9dd3fe5882
Melanie Fryman <melanie@frymanet.com>
2014-08-15 12:07:20 -0500

    added colors & pacman jQuery

---
+++ app/controllers/games.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.index = function(req, res){
+  res.render('home/index');
+};
+
+exports.colors = function(req, res){
+  res.render('games/colors');
+};
+
+exports.pacman = function(req, res){
+  res.render('games/pacman');
+};
---
+++ app/static/js/user/packman.js
@@ -0,0 +1,12 @@
+/* global $, document */
+
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    alert('Pacman is ready to play!');
+  });
+
+
+})();
+
